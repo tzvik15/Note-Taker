098bd6fcf54887ee38406e80274b794dad26b8db
tzvik15 <tzvik15@yahoo.com>
2019-11-20 11:53:44 -0700

    resolved array issue by calling the dbRead code within the dbWrite function, pushing, and then overwriting the existing JSON. Added a delete method that deletes the identified note from the JSON, then filteres the array to remove nulls, then write the new array to the JSON

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+let autoIterator = 1;
 
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"),(err, data) => {
+const db = path.join(__dirname, "/db/db.json")
+
+const dbRead = JSON.parse(fs.readFileSync(db,(err, data) => {
     if (err) throw err;
 }));
 
-const dbWrite = (newRes) => { return fs.writeFile(path.join(__dirname, "/db/db.json"),newRes, (err)=>{
+const dbWrite = (dbRead) => {
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"),JSON.stringify(dbRead), (err)=>{
     if (err) throw err;
 })
 }
+
+
 // Sets up the Express App
 // =============================================================
 const app = express();
@@ -24,26 +30,36 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("notes", function(req, res) {
+app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    return res.json(db);
+    return res.json(dbRead);
 });
 
 app.post("/api/notes", (req, res)=>{
-    let newRes = req.body;
-    
-    dbWrite(newRes);
-    
-    
-    console.log(newRes)
-
-    res.json(newRes);
-  
+    let newNote = req.body;
+    newNote.id = autoIterator;
+    autoIterator++;
+    dbRead.push(newNote);
+    dbWrite(dbRead);
+    return res.json(dbRead);
 })
 
+app.delete("/api/notes/:id" , (req,res)=>{
+    let id = req.params.id;
+    delete dbRead[id-1];
+    let filtered = dbRead.filter(function (el) {
+        return el != null;
+    });
+    dbWrite(filtered);
+    res.send(filtered);
+})
 
 
 
