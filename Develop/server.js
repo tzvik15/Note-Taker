7ed738fc2f9163b07226a29aa8f0e61f0b6dca59
tzvik15 <tzvik15@yahoo.com>
2019-11-21 11:36:02 -0700

    finished the app. added a conditional to the index.js file that will only render notes that are not null, preventing a jquery issue

--- Develop/server.js
+++ Develop/server.js
@@ -3,18 +3,28 @@ const path = require("path");
 const fs = require("fs");
 let autoIterator = 1;
 
-const db = path.join(__dirname, "/db/db.json")
+const db = path.join(__dirname, "/db/db.json");
 
-const dbRead = JSON.parse(fs.readFileSync(db,(err, data) => {
+//a process for reading the JSON file containing the array of existing note.
+const dbRead = JSON.parse(
+  fs.readFileSync(db, (err, data) => {
     if (err) throw err;
-}));
+  })
+);
 
-const dbWrite = (dbRead) => {
-    fs.writeFileSync(path.join(__dirname, "/db/db.json"),JSON.stringify(dbRead), (err)=>{
-    if (err) throw err;
-})
-}
-
+//a function that reads the existing json file (using the above process), filteres out any "null"s, and writes the filtered array back to the json
+const dbWrite = dbRead => {
+  let filtered = dbRead.filter(function(el) {
+    return el != null;
+  });
+  fs.writeFileSync(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(filtered),
+    err => {
+      if (err) throw err;
+    }
+  );
+};
 
 // Sets up the Express App
 // =============================================================
@@ -27,52 +37,40 @@ app.use(express.static("public"));
 
 //server processes
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    return res.json(dbRead);
+  return res.json(dbRead);
 });
 
-app.post("/api/notes", (req, res)=>{
-    let newNote = req.body;
-    newNote.id = autoIterator;
-    autoIterator++;
-    dbRead.push(newNote);
-    dbWrite(dbRead);
-    return res.json(dbRead);
-})
+//the post method gets the information from the body of the submitted note, adds an id to the note corresponding to an automatically incrementing variable (so each note gets a unique, numbered id), pushes the newnote into the array that dbRead reads (the json file), and callbacks the write function, thus creating a new json file which includes the new note data.
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = autoIterator;
+  autoIterator++;
+  dbRead.push(newNote);
+  dbWrite(dbRead);
+  return res.json(dbRead);
+});
 
-app.delete("/api/notes/:id" , (req,res)=>{
-    let id = req.params.id;
-    delete dbRead[id-1];
-    let filtered = dbRead.filter(function (el) {
-        return el != null;
-    });
-    dbWrite(filtered);
-    res.send(filtered);
-})
-
-
-
-
-
-
-
-
-
-
-
+//the process for deleting a note by note id. Since the notes have id's starting with 1, but the json file is an array (so starting at index 0), the deleted note is actually set to id-1 - to correspond to the way they are stored in the json array
+app.delete("/api/notes/:id", (req, res) => {
+  let id = req.params.id;
+  delete dbRead[id - 1];
+  dbWrite(dbRead);
+  res.send(dbRead);
+});
 
 //activate the server
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
